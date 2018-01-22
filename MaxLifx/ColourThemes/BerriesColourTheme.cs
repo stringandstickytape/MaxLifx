d097bcc575578ab5a28183bf56c1decc9500283c
Max <Max@example.com>
2018-01-22 12:53:53 +0000

    Add two new colour themes.

---
+++ MaxLifx/ColourThemes/BerriesColourTheme.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxLifx.ColourThemes
+{
+    public class BerriesColourTheme : IColourTheme
+    {
+        public void SetColours(Random r, List<int> hues, List<int> hueRanges, List<double> saturations, List<double> saturationRanges, List<float> brightnesses, List<float> brightnessRanges, bool pastel, bool lockBrightness)
+        {
+            for (int index = 0; index < hues.Count; index++)
+            {
+                switch (r.Next(3))
+                {
+                    case 0:
+                        hues[index] = r.Next(20) - 10; // red
+                        break;
+                    case 1:
+                        hues[index] = r.Next(30) + 225; // blue
+                        break;
+                    case 2:
+                        hues[index] = r.Next(20) + 290; // purple
+                        break;
+                }
+                
+            }
+
+            for (int index = 0; index < hueRanges.Count; index++)
+                hueRanges[index] = r.Next(20);
+
+            for (int index = 0; index < saturations.Count; index++)
+                saturations[index] = r.NextDouble() / 10 + .9f;
+
+            for (int index = 0; index < saturationRanges.Count; index++)
+                saturationRanges[index] = r.NextDouble() / 10;
+
+            if (!lockBrightness)
+                for (int index = 0; index < brightnesses.Count; index++)
+                brightnesses[index] = (float)(r.NextDouble()/10+.9f);
+
+            if (!lockBrightness)
+                for (int index = 0; index < brightnessRanges.Count; index++)
+                brightnessRanges[index] = (float)(r.NextDouble() / 10);
+
+            if (pastel)
+            {
+                for (int index = 0; index < saturations.Count; index++)
+                    saturations[index] = saturations[index]/2;
+
+                for (int index = 0; index < brightnesses.Count; index++)
+                    brightnesses[index] = (brightnesses[index] * 2 < 1f ? brightnesses[index] * 2 : 1f);
+            }
+        }
+    }
+}
---
+++ MaxLifx/ColourThemes/BlackAndWhite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxLifx.ColourThemes
+{
+    public class BlackAndWhiteColourTheme : IColourTheme
+    {
+        public void SetColours(Random r, List<int> hues, List<int> hueRanges, List<double> saturations, List<double> saturationRanges, List<float> brightnesses, List<float> brightnessRanges, bool pastel, bool lockBrightness)
+        {
+            for (int index = 0; index < hues.Count; index++)
+            {
+                        hues[index] = 0; // red
+                        break;
+            }
+
+            for (int index = 0; index < hueRanges.Count; index++)
+                hueRanges[index] = 0;
+
+            for (int index = 0; index < saturations.Count; index++)
+                saturations[index] = 0f;
+
+            for (int index = 0; index < saturationRanges.Count; index++)
+                saturationRanges[index] = 0;
+
+            if (!lockBrightness)
+                for (int index = 0; index < brightnesses.Count; index++)
+                brightnesses[index] = pastel ? .75f : .5f;
+
+            if (!lockBrightness)
+                for (int index = 0; index < brightnessRanges.Count; index++)
+                brightnessRanges[index] = pastel ? .25f : .5f;
+        }
+    }
+}
